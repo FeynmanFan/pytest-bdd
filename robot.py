7fd5a62f2f23409ab6d19d41dc22aed9b447477b
FeynmanFan <cbehrens@gmail.com>
2023-08-07 18:49:46 -0500

    Removed working code for demo

--- robot.py
+++ robot.py
@@ -3,12 +3,8 @@ import velocity
 class robot:
     def __init__(self, velocity):
         self.velocity = velocity;
-        print("init")
 
     def motionEvaluation(self, nearfieldDetected = False):
         # do a bunch of stuff that is unrelated to the current issue
-        if nearfieldDetected:
-            self.velocity.speed -= 1
-        else:
             pass            
     
