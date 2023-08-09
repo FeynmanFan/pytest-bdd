87fe582adebbcf7ea917e8cd42159dd98cda2032
FeynmanFan <cbehrens@gmail.com>
2023-08-09 16:13:51 -0500

    Added logging for givens

--- tests/test_robotslowsdown1.py
+++ tests/test_robotslowsdown1.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from pytest_bdd import scenario, given, when, then, parsers
 from robot import robot
@@ -20,12 +21,14 @@ def test_slowdown():
 	return
     
 @given("the near field sensor detects an object", target_fixture="theNearFieldSensor")
-def sensor_fired(theNearFieldSensor, ):
+def sensor_fired(theNearFieldSensor):
+	print('nearfieldsensor' + '\t\t' + str(time.time()))
 	theNearFieldSensor = True
 	return theNearFieldSensor
 
 @given(parsers.cfparse("the robot is traveling at {initialSpeed: d}m/s"))
 def robot_travelling(the_robot, initialSpeed):
+	print('initialSpeed' + '\t\t' + str(time.time()))
 	the_robot.velocity.speed = initialSpeed
 
 @when("motion evaluation fires")
