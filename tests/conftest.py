a8ed329b7345e8825e3d0dbd50101684de99b623
FeynmanFan <cbehrens@gmail.com>
2023-08-14 17:21:48 -0500

    Sketchy data-driven approach

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,36 @@
 #conftest.py
+import pandas as pd
+import os
 
 def pytest_bdd_before_scenario(request, feature, scenario):
-    #print("Before_scenario")
-    #print("Feature: " + feature.name)
-    #print("Scenario: " + scenario.name)
-    pass
+    if ("data-driven" not in scenario.tags):
+        return
+    
+    dataFile = ''
+
+    print('\n\nscenario:' + str(vars(scenario)))
+
+    for tag in scenario.tags:
+        #print (str(tag))
+        if (tag.startswith("datafile_")):
+            dataFile = tag.split('_')[1]
+
+    if (dataFile == ''):
+        return
+
+    dir, null = os.path.split(str(request.path))
+    fileName = dir + "\\resources\\" + dataFile
+
+    df = pd.read_excel(io=fileName, sheet_name="data")
+
+    examples = scenario.feature.scenarios[scenario.name].examples.examples
+
+    # clear existing examples
+    examples = []
+    for index, row in df.iterrows():
+        examples.append({row[0], row[1]})
+
+    scenario.feature.scenarios[scenario.name].examples.examples = examples
 
 def pytest_bdd_after_step(request, feature, scenario, step, step_func, step_func_args):
     #print('whatever is going on')
